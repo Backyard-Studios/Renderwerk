ba15b53f6c94fce2b87bd1e48463cb73b457102c
Drischdaan <drischdaan@gmail.com>
2024-09-11 01:14:00 +0200

    chore(workspace): adds comment to properties file

--- Scripts/Config/properties.lua
+++ Scripts/Config/properties.lua
@@ -1,3 +1,18 @@
+-- []=========================================================================[]
+-- Note: This file is used to define properties that are used by the build
+-- system. Please DO NOT modify the properties_preprocessed.lua file directly.
+-- Instead modify this file and run the setup script to regenerate it.
+--
+-- You can use the following variables in this file:
+--   - $(PROJECT_NAME) - The name of the project.
+--   - $(PROJECT_NAME_SHORTHAND) - The shorthand uppercase name of the project.
+--   - $(PROJECT_VERSION) - The version of the project.
+--   - $(PROJECT_VERSION_SUFFIX) - The version suffix of the project.
+--   - $(PROJECT_VERSION_STRING) - The version string of the project.
+--   - $(PROJECT_AUTHOR) - The author of the project.
+--   - $(ROOT_PATH) - The root path of the project.
+-- []=========================================================================[]
+
 project_name = '$(PROJECT_NAME)'
 project_version = '$(PROJECT_VERSION)'
 project_version_suffix = '$(PROJECT_VERSION_SUFFIX)'
