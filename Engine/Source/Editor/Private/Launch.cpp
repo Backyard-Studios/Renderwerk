de5f3b7fb5c0a7e71ff3d5c761589f08b68e5549
Drischdaan <drischdaan@gmail.com>
2024-09-11 23:14:31 +0200

    chore(engine): removes test code

--- Engine/Source/Editor/Private/Launch.cpp
+++ Engine/Source/Editor/Private/Launch.cpp
@@ -1,11 +1,6 @@
 #include "pch.h"
 
-#include <cstdio>
-
-#include "Test.h"
-
 int main()
 {
-	printf("TestFunction(42): %d\n", TestFunction(42));
 	return 0;
 }
--- Engine/Source/Engine/Public/Test.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "Renderwerk/Core/CoreDefinitions.h"
-
-ENGINE_API int TestFunction(int InValue);
