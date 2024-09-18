77c158e8f08c091a1b5287d8d949f90dd323f7ad
Drischdaan <drischdaan@gmail.com>
2024-09-18 17:32:24 +0200

    feat(engine): adds window manager (#RW-27)

--- Engine/Source/Engine/Public/Renderwerk/Engine/Engine.h
+++ Engine/Source/Engine/Public/Renderwerk/Engine/Engine.h
@@ -4,6 +4,7 @@
 #include "Renderwerk/Core/Result.h"
 
 #include "Renderwerk/Memory/SharedPointer.h"
+#include "Renderwerk/Platform/WindowManager.h"
 
 class ENGINE_API FEngine
 {
@@ -21,11 +22,18 @@ private:
 	FResult RunLoop();
 	void Shutdown();
 
+public:
+	[[nodiscard]] TSharedPointer<FWindowManager> GetWindowManager() const { return WindowManager; }
+	[[nodiscard]] TSharedPointer<IWindow> GetMainWindow() const { return MainWindow; }
+
 private:
 	bool bIsAlreadyLaunched = false;
 	bool bIsAlreadyShutdown = false;
 
 	bool bIsShutdownRequested = false;
+
+	TSharedPointer<FWindowManager> WindowManager;
+	TSharedPointer<IWindow> MainWindow;
 
 	friend void Shutdown();
 };
