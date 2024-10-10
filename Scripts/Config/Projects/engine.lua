650d6793e5abd6212274defcabd7b242c08ba0cc
Drischdaan <drischdaan@gmail.com>
2024-10-10 17:54:32 +0200

    build(workspace): adds imgui project

--- Scripts/Config/Projects/engine.lua
+++ Scripts/Config/Projects/engine.lua
@@ -13,6 +13,7 @@ rw_project('Engine')
 	rw_link_project('tracy', rw_make_third_party_location(path.join('tracy', 'public')))
 	rw_link_project('spdlog', rw_make_third_party_location(path.join('spdlog', 'include')))
 	rw_link_project('flecs', rw_make_third_party_location(path.join('flecs', 'include')))
+	rw_link_project('imgui', rw_make_third_party_location(path.join('imgui')))
 
 	defines({
 		'TRACY_ENABLE',
