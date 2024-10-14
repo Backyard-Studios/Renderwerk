dd0caeae25870ce8162be9018cf3c6646d1a04d8
Drischdaan <drischdaan@gmail.com>
2024-10-14 10:12:57 +0200

    feat(engine): adds fence abstraction

---
+++ Engine/Source/Engine/Private/Renderwerk/Renderer/Fence.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+
+#include "Renderwerk/Renderer/Fence.h"
+
+FFence::FFence(const ComPtr<ID3D12Device14>& Device)
+	: LastSignaledValue(0)
+{
+	CHECK_RESULT(Device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&Fence)), "Failed to create fence")
+	FenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+}
+
+FFence::~FFence()
+{
+	if (FenceEvent)
+		CloseHandle(FenceEvent);
+	Fence.Reset();
+}
+
+void FFence::Signal(const ComPtr<ID3D12CommandQueue>& CommandQueue)
+{
+	++LastSignaledValue;
+	CHECK_RESULT(CommandQueue->Signal(Fence.Get(), LastSignaledValue), "Failed to signal fence");
+}
+
+void FFence::WaitCPU(const uint64 Timeout) const
+{
+	if (IsValueComplete(LastSignaledValue))
+		return;
+	CHECK_RESULT(Fence->SetEventOnCompletion(LastSignaledValue, FenceEvent), "Failed to set event on completion")
+	DWORD WaitResult = WaitForSingleObject(FenceEvent, static_cast<DWORD>(Timeout));
+	RW_DEBUG_ASSERT(WaitResult == WAIT_OBJECT_0, "Failed to wait for fence");
+}
+
+void FFence::WaitGPU(const ComPtr<ID3D12CommandQueue>& CommandQueue, const uint64 Value) const
+{
+	CHECK_RESULT(CommandQueue->Wait(Fence.Get(), Value), "Failed to wait for fence")
+}
+
+uint64 FFence::GetLastCompletedValue() const
+{
+	return Fence->GetCompletedValue();
+}
+
+bool8 FFence::IsValueComplete(const uint64 InValue) const
+{
+	return Fence->GetCompletedValue() >= InValue;
+}
---
+++ Engine/Source/Engine/Public/Renderwerk/Renderer/Fence.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreMinimal.h"
+#include "Renderwerk/Renderer/D3D12Include.h"
+
+class ENGINE_API FFence
+{
+public:
+	FFence(const ComPtr<ID3D12Device14>& Device);
+	~FFence();
+
+	DELETE_COPY_AND_MOVE(FFence);
+
+public:
+	void Signal(const ComPtr<ID3D12CommandQueue>& CommandQueue);
+
+	void WaitCPU(uint64 Timeout = INFINITE) const;
+	void WaitGPU(const ComPtr<ID3D12CommandQueue>& CommandQueue, uint64 Value) const;
+
+	[[nodiscard]] uint64 GetLastCompletedValue() const;
+	[[nodiscard]] bool8 IsValueComplete(uint64 InValue) const;
+
+public:
+	[[nodiscard]] ComPtr<ID3D12Fence1> GetHandle() const { return Fence; }
+
+	[[nodiscard]] uint64 GetLastSignaledValue() const { return LastSignaledValue; }
+
+private:
+	ComPtr<ID3D12Fence1> Fence;
+	HANDLE FenceEvent;
+
+	uint64 LastSignaledValue;
+};
